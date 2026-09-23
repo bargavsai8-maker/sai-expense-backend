07039bcbadf83fc9a7b049390d329fc1cdf5dddf
SAI GANESH <bargavsai8@gmail.com>
2026-09-23 07:15:00 +0530

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("SAI Expense Tracker Backend is running!");
+});
+
+app.post("/api/transactions", (req, res) => {
+  const transaction = req.body;
+
+  console.log("New transaction:", transaction);
+
+  res.json({
+    success: true,
+    message: "Transaction received!",
+    transaction: transaction
+  });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
